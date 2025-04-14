e9a08ac69a5ccded28eba921a380fb2915e3bb0d
Nash Maglaqui <nashmglq89@gmail.com>
2025-04-14 19:35:31 +0800

    fix: Removed Files

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-
+// Passport is for authentication
 passport.use(
   new GoogleStrategy(
     {
